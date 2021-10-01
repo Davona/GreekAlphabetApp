4e3a303016244bef739df4e1bf3fd8d9be33f0c8
Davith Shamiryan <dshamiryan@list.ru>
2021-10-01 04:12:16 +0400

    Add project files.

---
+++ GreekAlphabetApp/GreekAlphabet.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreekAlphabetApp
+{
+    class GreekAlphabet
+    {
+        private string[] alphabet = new string[]
+        { "alpha",
+          "beta",
+          "gamma",
+          "delta",
+          "epsilon",
+          "stigma",
+          "zeta",
+          "eta",
+          "theta",
+          "iota" 
+        };
+        private int GetIndexOfLetter(string key) 
+        {
+            int defaultIndex = 0;
+            for (int i = 0; i < alphabet.Length; i++)
+            {
+                if (alphabet[i]==key)
+                {
+                    defaultIndex = i;
+                }
+               
+            }
+            defaultIndex++;
+            return defaultIndex;
+        }
+        private string GetLetter(int index) 
+        {
+            string letter = "";
+            for (int i = 0; i < alphabet.Length; i++)
+            {
+                if (i+1==index)
+                {
+                    letter = alphabet[i];
+
+                }
+            }
+            return letter;
+        
+        
+        }
+        public int this[string  key]
+        {
+            get { return GetIndexOfLetter(key); }
+           
+        }
+        public string this[int index]
+        {
+            get { return GetLetter(index); }
+           
+        }
+    }
+}
---
+++ GreekAlphabetApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreekAlphabetApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GreekAlphabet alphabet = new GreekAlphabet();
+            int numberOfLetter = alphabet["delta"];
+            Console.WriteLine(numberOfLetter);
+            string letter = alphabet[3];
+            Console.WriteLine(letter);
+        }
+    }
+}
